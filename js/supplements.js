a8ab7e8158865e587a647412cf6537de2d1498fe
GaryB96 <garybryson02@gmail.com>
2025-08-29 13:46:42 -0300

    Update supplements.js

--- js/supplements.js
+++ js/supplements.js
@@ -1,1 +1,9 @@
+// supplements.js
+import { db } from "./firebaseConfig.js";
+import { collection, getDocs, addDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
+export async function fetchSupplements(userId) {
+  const supplementsRef = collection(db, "users", userId, "supplements");
+  const snapshot = await getDocs(supplementsRef);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
