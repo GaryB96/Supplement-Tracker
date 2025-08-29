7292bbdaf3748447154f5be924ded7938dbdfdab
GaryB96 <garybryson02@gmail.com>
2025-08-29 18:39:05 -0300

    Update supplements.js

--- js/supplements.js
+++ js/supplements.js
@@ -1,5 +1,12 @@
+// supplements.js
 import { db } from "./firebaseConfig.js";
-import { collection, getDocs, addDoc, deleteDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
 export async function fetchSupplements(userId) {
   const supplementsRef = collection(db, "users", userId, "supplements");
